2d8a8ac7b072a057b0eb157f3324d5bb164f895c
Kevin Morris <kmorris511@gmail.com>
2024-01-25 22:25:01 -0700

    implmenting more 3270 constants

--- Services/Constants.cs
+++ Services/Constants.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-// ReSharper disable InconsistentNaming
+﻿// ReSharper disable InconsistentNaming
 
 namespace Services
 {
@@ -22,14 +17,34 @@ namespace Services
 
     public static class Commands
     {
+        public const byte WRITE = 0xf1;
+        public const byte ERASE_WRITE = 0xf5;
+        public const byte READ_BUFFER = 0xf2;
+        public const byte READ_MODIFIED = 0xf6;
+        public const byte READ_MODIFIED_ALL = 0x6e;
+        public const byte ERASE_ALL_UNPROTECTED = 0x6f;
+        public const byte WRITE_STRUCTURED_FIELD = 0xf3;
     }
 
     public static class WCC
     {
+        public const byte RESET = 0b0100_0000;
+        public const byte KEYBOARD_RESTORE = 0b0000_0010;
+        public const byte RESET_MDT = 0b0000_0001;
     }
 
     public static class Orders
     {
+        public const byte START_FIELD = 0x1d;
+        public const byte START_FIELD_EXTENDED = 0x29;
+        public const byte SET_BUFFER_ADDRESS = 0x11;
+        public const byte SET_ATTRIBUTE = 0x28;
+        public const byte MODIFY_FIELD = 0x2c;
+        public const byte INSERT_CURSOR = 0x13;
+        public const byte PROGRAM_TAB = 0x05;
+        public const byte REPEAT_TO_ADDRESS = 0x3c;
+        public const byte ERASE_UNPROTECTED_TO_ADDRESS = 0x12;
+        public const byte GRAPHIC_ESCAPE = 0x08;
     }
 
     public static class AID
--- Services/RowHandler.cs
+++ Services/RowHandler.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using Util;
 
 namespace Services
 {
     public class RowHandler<T>(I3270Translator<T> translator)
     {
-        public byte[] Buffer { get; } = new byte[80];
+        public byte[] Buffer { get; } = new byte[Constants.SCREEN_WIDTH];
         public bool Dirty { get; private set; } = false;
         public event EventHandler<RowUpdateEventArgs<T>>? RowUpdated;
 
@@ -23,8 +24,7 @@ namespace Services
         {
             if (Dirty)
             {
-                var rowUpdated = RowUpdated;
-                rowUpdated?.Invoke(this, new RowUpdateEventArgs<T>()
+                RowUpdated?.Invoke(this, new RowUpdateEventArgs<T>()
                 {
                     Data = translator.Translate(Buffer)
                 });
