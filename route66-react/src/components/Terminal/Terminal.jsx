02347b30bb411da3a12ccc95fe00cd809dd7e338
Kevin Morris <kmorris511@gmail.com>
2025-05-26 16:29:09 -0600

    Adding function keys

--- route66-react/src/components/Terminal/Terminal.jsx
+++ route66-react/src/components/Terminal/Terminal.jsx
@@ -28,9 +28,39 @@ export default function Terminal({websocket: {sendJsonMessage, lastJsonMessage, 
         processMessage(lastJsonMessage, setSessionKey, processRow(setFieldData))
     }, [lastJsonMessage]);
 
+    function functionKey(key) {
+
+    }
+
     const rows = rowRange.map(i => <Row i={i}
                                         fieldData={fieldData[i]}
                                         inputChanged={inputValueChanged(setFieldData)}
                                         focusChanged={setCursor}></Row>)
-    return (<div>{rows}</div>);
+
+    return (
+        <div id="container" className="border-zero">
+            <div>
+                <button className="keypad-button">Reset</button>
+                <button className="keypad-button" onClick={() => functionKey(Constants.AID.PA1)}>PA1</button>
+                <button className="keypad-button" onClick={() => functionKey(Constants.AID.PA2)}>PA2</button>
+                <button className="keypad-button" onClick={() => functionKey(Constants.AID.PA3)}>PA3</button>
+                <button className="keypad-button" onClick={() => functionKey(Constants.AID.SYSREQ)}>Sys Req</button>
+                <button className="keypad-button" onClick={() => functionKey(Constants.AID.CLEAR)}>Clear</button>
+            </div>
+            <div>
+                <button className="keypad-button" onClick={() => functionKey(Constants.AID.PF1)}>PF1</button>
+                <button className="keypad-button" onClick={() => functionKey(Constants.AID.PF2)}>PF2</button>
+                <button className="keypad-button" onClick={() => functionKey(Constants.AID.PF3)}>PF3</button>
+                <button className="keypad-button" onClick={() => functionKey(Constants.AID.PF4)}>PF4</button>
+                <button className="keypad-button" onClick={() => functionKey(Constants.AID.PF5)}>PF5</button>
+                <button className="keypad-button" onClick={() => functionKey(Constants.AID.PF6)}>PF6</button>
+                <button className="keypad-button" onClick={() => functionKey(Constants.AID.PF7)}>PF7</button>
+                <button className="keypad-button" onClick={() => functionKey(Constants.AID.PF8)}>PF8</button>
+                <button className="keypad-button" onClick={() => functionKey(Constants.AID.PF9)}>PF9</button>
+                <button className="keypad-button" onClick={() => functionKey(Constants.AID.PF10)}>PF10</button>
+                <button className="keypad-button" onClick={() => functionKey(Constants.AID.PF11)}>PF11</button>
+                <button className="keypad-button" onClick={() => functionKey(Constants.AID.PF12)}>PF12</button>
+            </div>
+            <div>{rows}</div>
+        </div>);
 }
